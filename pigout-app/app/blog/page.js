45d0e5110e3407b8efcd4560107082189d5dcb74
mnadiger <mariyanna123@gmail.com>
2025-08-13 10:16:21 +0530

    changed type

--- pigout-app/app/blog/page.js
+++ pigout-app/app/blog/page.js
@@ -27,8 +27,8 @@ function BlogPostPage() {
     <br/>
 
     <Link href={"/blog/post-2"} style={linkStyle}>Go to Post 2</Link>
-<br />
-        <Link href={"/blog/anything-else"} style={linkStyle}>Go to Anything Else</Link>
+        <br />
+    <Link href={"/blog/anything-else"} style={linkStyle}>Go to Anything Else</Link>
 
         <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white' }}> back to <a href="/" style={{ color: '#fff', textDecoration: 'underline' }}> Blog</a></p>
 
